d898bbf7ed77fc9402074ae3a49e1d72ceed62a4
Gabor Sajo <gabor@voyagerweb.ca>
2026-03-05 16:35:28 -0700

    Min os versions

--- Package.swift
+++ Package.swift
@@ -5,6 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "Cache",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6),
+        .visionOS(.v1)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
