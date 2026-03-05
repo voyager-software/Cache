432d20db6c6b7f95a4de0e19590678cbdde807b9
Gabor Sajo <gabor@voyagerweb.ca>
2026-03-05 16:36:51 -0700

    Fix the Expiry never case

--- Sources/Cache/Library/Expiry.swift
+++ Sources/Cache/Library/Expiry.swift
@@ -15,8 +15,7 @@ public enum Expiry: Sendable {
   public var date: Date {
     switch self {
     case .never:
-      // Ref: http://lists.apple.com/archives/cocoa-dev/2005/Apr/msg01833.html
-      return Date(timeIntervalSince1970: 60 * 60 * 24 * 365 * 68)
+      return Date.distantFuture
     case .seconds(let seconds):
       return Date().addingTimeInterval(seconds)
     case .date(let date):
